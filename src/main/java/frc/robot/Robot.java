6c5cda52434cea28b51324cfc60e66e50cc08f7b
UserC2 <85082371+UserC2@users.noreply.github.com>
2025-11-20 23:14:31 -0500

    Use DifferentialDrive class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,22 +1,24 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
-
+  
   // Master motors
   private final WPI_TalonSRX leftMaster = new WPI_TalonSRX(1);
   private final WPI_TalonSRX rightMaster = new WPI_TalonSRX(3);
-
+  
   // Followers
   private final WPI_TalonSRX leftFollower = new WPI_TalonSRX(2);
   private final WPI_TalonSRX rightFollower = new WPI_TalonSRX(4);
-
+  
   private final XboxController controller = new XboxController(0);
+  
+  private final DifferentialDrive differentialDrive = new DifferentialDrive(leftMaster, rightMaster);
 
   @Override
   public void robotInit() {
@@ -36,16 +38,7 @@ public class Robot extends TimedRobot {
     // Right stick X = turning
     double rotation = controller.getRightX();
 
-    // Arcade math
-    double leftPower = throttle + rotation;
-    double rightPower = throttle - rotation;
-
-    // Clamp to [-1, 1]
-    leftPower = Math.max(-1, Math.min(1, leftPower));
-    rightPower = Math.max(-1, Math.min(1, rightPower));
-
-    // Send it
-    leftMaster.set(ControlMode.PercentOutput, leftPower);
-    rightMaster.set(ControlMode.PercentOutput, rightPower);
+    differentialDrive.setDeadband(0.05);
+    differentialDrive.arcadeDrive(throttle, rotation, false);
   }
 }
