57c5b08161bad7a2d3d7b4828906b15e6830422d
UserC2 <85082371+UserC2@users.noreply.github.com>
2025-11-21 01:24:26 -0500

    Simulation

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -6,8 +6,17 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,6 +28,13 @@ public class Drive extends SubsystemBase {
   private final WPI_TalonSRX rightFollower = new WPI_TalonSRX(4);
 
   private final DifferentialDrive differentialDrive = new DifferentialDrive(leftMaster, rightMaster);
+  
+  // TODO: the gear ratio is almost certainly not correct
+  private final DifferentialDrivetrainSim drivetrainSim = DifferentialDrivetrainSim.createKitbotSim(KitbotMotor.kDualCIMPerSide,  KitbotGearing.k8p45, KitbotWheelSize.kSixInch, null);
+  private final Field2d field = new Field2d();
+  private final Timer timer = new Timer();
+
+  private final DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(Rotation2d.kZero, 0, 0);
 
   /** Creates a new Drive. */
   public Drive() {
@@ -31,11 +47,23 @@ public class Drive extends SubsystemBase {
     rightFollower.setInverted(true);
 
     differentialDrive.setDeadband(0.05);
+
+    SmartDashboard.putData(field);
+
+    timer.start();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    // This simulation code is intentionally not in simulationPeriodic()
+    drivetrainSim.setInputs(leftMaster.getMotorOutputVoltage(), rightMaster.getMotorOutputVoltage());
+    drivetrainSim.update(timer.get());
+    timer.reset();
+    
+    odometry.update(drivetrainSim.getHeading(), drivetrainSim.getLeftPositionMeters(), drivetrainSim.getRightPositionMeters());
+    field.setRobotPose(odometry.getPoseMeters());
   }
 
   public Command getTeleopCommand(XboxController controller) {
